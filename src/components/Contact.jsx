2d09dd13c7035545a4a88afb40500f37d119567c
slakshya-22 <slakshyamainacc@gmail.com>
2025-02-23 19:40:17 +0530

    Fixed emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 const Contact = () => {
   const formRef = useRef();
@@ -10,19 +11,31 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_r8v7d3q",   
-        "template_68yx7ja",  
+        "service_r8v7d3q",
+        "template_c0p0c46",
         formRef.current,
-        "fvGDGRA53A12Osoj7"   
+        "fvGDGRA53A12Osoj7"
       )
       .then(
         (result) => {
           console.log("Success:", result.text);
+          Swal.fire({
+            title: "Success!",
+            text: "Message sent successfully",
+            icon: "success",
+            confirmButtonColor: "#4CAF50",
+          });
           setIsSent(true);
-          formRef.current.reset(); 
+          formRef.current.reset();
         },
         (error) => {
           console.error("Error:", error.text);
+          Swal.fire({
+            title: "Error!",
+            text: error.text,
+            icon: "error",
+            confirmButtonColor: "#D32F2F",
+          });
         }
       );
   };
@@ -43,7 +56,7 @@ const Contact = () => {
           <label className="text-white block mb-2">Your Message</label>
           <textarea name="message" rows="5" required className="w-full p-3 rounded bg-gray-800 text-white outline-none"></textarea>
         </div>
-        <button type="submit" className="w-full bg-gradient-to-r from-blue-500 to-pink-500 text-white p-3 rounded-md hover:opacity-90 transition-all">
+        <button type="submit" className="cursor-pointer w-full bg-gradient-to-r from-blue-500 to-pink-500 text-white p-3 rounded-md hover:opacity-90 transition-all duration-200">
           Send Message
         </button>
         {isSent && <p className="text-green-500 mt-4">Message sent successfully!</p>}
